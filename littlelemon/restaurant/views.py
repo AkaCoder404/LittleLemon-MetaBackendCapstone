a385edf6068bfff2d304a39cd6b9c0712a6ee3bd
akacoder404 <akacoder404@outlook.com>
2023-09-15 14:55:10 +0800

    adding apis

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,3 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
+from .models import Menu, Booking
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
